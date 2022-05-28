57c7bb30c930273ad64ecd9ef4fc642883be3fea
Daniel Goman <gomanGGGoman@gmail.com>
2022-05-28 22:22:00 +0300

    added run command

--- src/main.py
+++ src/main.py
@@ -5,18 +5,33 @@ from src.utils.args_handler import get_args
 from src.params import IS_HIDDEN_RUN
 
 
+# TODO: fix missing some songs during the download
 def main():
+    """Downloads a list of songs, where the input is a directory of .txt files containing \n separated names of songs
+    Expects the input to be in the directory SongDownloader/input as a set of .txt files, containing one song name
+    in a row
+
+    Currently downloads everything into 'C:\Users\GoMaN\Downloads'
+
+    for WSL:
+        PYTHONPATH=. python3 src/main.py
+
+    for Windows:
+        set PYTHONPATH=.
+        python src.main.py
+
+    """
     input_dir, output_dir = get_args()
 
     setup_logger()
 
-    songs_names = get_input_file_names(input_path=input_dir)
+    songs_names = get_input_file_names(input_dir_path=input_dir)
 
     driver = config_driver(is_hidden_run=IS_HIDDEN_RUN)
 
-    downloaded_songs = download_songs(driver=driver, songs=songs_names)
+    song_file_names = download_songs(driver=driver, songs=songs_names)
 
-    move_songs_to_output_dir(song_file_names=downloaded_songs, output_dir=output_dir)
+    move_songs_to_output_dir(song_file_names=song_file_names, output_dir=output_dir)
 
     driver.quit()
 
